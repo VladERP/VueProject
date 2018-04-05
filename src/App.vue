a0f03d03a2c27ec21d350170ef3de4c02d8b3812
VladERP <37039112+VladERP@users.noreply.github.com>
2018-04-05 17:17:45 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a href="#" class="nav-link">Главная</a>
+            </li>
+            <li class="nav-item">
+                <a href="reference.html" class="nav-link">Справка</a>
+            </li>
+            <li class="nav-item">
+                <a href="#myModal" class="btn btn-primary" data-toggle="modal">Создать задачу</a> 
+            </li> 
+        </ul>
     </div>
+    <div class="user-wrap dropdown">
+    <a class="nav-link dropdown-toggle navbar-toggler-right user-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="img/avatar.jfif" alt="Иванов И.И." class="user-img">Иванов И.И.
+       <span class="user-staf">Программист</span> </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="PersonalArea.html">Личный кабинет</a>
+          <a class="dropdown-item" href="#">Выход</a>
+        </div>
+    </div> 
+    </div>   
+</nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass">
+
 </style>
+<script>
+
+
+</script>
